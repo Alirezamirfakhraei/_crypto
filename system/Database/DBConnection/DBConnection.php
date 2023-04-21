1e3b57caa803b09df5ee94f8f79a461133621f7e
Alirezamirfakhraei <alimirireza70@gmail.com>
2023-04-21 18:36:30 +0430

    start ORM system in project

---
+++ system/Database/DBConnection/DBConnection.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace System\Database\DBConnection;
+use PDO;
+
+class DBConnection
+{
+    private static $dbConnectionInstanse = null;
+
+
+    private function __construct()
+    {
+
+    }
+
+    private static function getDBConnectionInstanse()
+    {
+        if (self::$dbConnectionInstanse == null){
+            $DBConnectionInstanse = new DBConnection();
+            self::$dbConnectionInstanse = $DBConnectionInstanse->dbConnection();
+
+        }
+        return self::$dbConnectionInstanse;
+    }
+
+    private function dbConnection()
+    {
+        $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION , PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+        try {
+            return new PDO("mysql:host=".DBHOST.";dbname=".DBNAME,DBUSERNAME , DBPASSWORD , $option);
+        }catch (\PDOException $exception){
+            echo "error in connect to database".$exception->getMessage();
+            return false;
+        }
+    }
+
+    public function newInsertID()
+    {
+        return self::getDBConnectionInstanse()->lastInsertId();
+    }
+}
