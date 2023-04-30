3403e00d9a037a06405c9ae91307fac8617e78cf
Alirezamirfakhraei <alimirireza70@gmail.com>
2023-04-30 21:31:51 +0430

    create collection recorde

--- system/Database/Traits/HasAttributes.php
+++ system/Database/Traits/HasAttributes.php
@@ -12,14 +12,43 @@ trait HasAttributes
 
     }
 
-    private function inCastsAttributes()
+    protected function arrayToAttribute(array $array, $object = null)
     {
+        if (!$object) {
+            $className = get_called_class();
+            $object = new $className;
+        }
+        foreach ($array as $attribute => $value) {
+            if ($this->inHiddenAttribute($attribute))
+                continue;
+            $this->registerAttribute($attribute, $object, $value);
+        }
+        return $object;
+    }
 
+    private function arrayToObjects(array $array)
+    {
+        $collection = [];
+        foreach ($array as $value) {
+            $object = $this->arrayToAttribute($value);
+            $collection[] = $object;
+        }
+        $this->colloction = $collection;
+    }
+
+    private function inHiddenAttribute($attribute)
+    {
+        return in_array($attribute, $this->hidden);
+    }
+
+    private function inCastsAttributes($attribute)
+    {
+        return in_array($attribute, array_keys($this->casts));
     }
 
     private function castsDecodeValue(string $attribute, $value)
     {
-        
+
     }
 
 }
