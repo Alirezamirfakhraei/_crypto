4357b3219a5a5b00e0ea97fdbb884114f32dd9e9
Alirezamirfakhraei <alimirireza70@gmail.com>
2023-05-06 22:17:11 +0430

    start has crud

--- system/Database/Traits/HasCRUD.php
+++ system/Database/Traits/HasCRUD.php
@@ -2,7 +2,39 @@
 
 namespace System\Database\Traits;
 
+use System\Database\DBConnection\DBConnection;
+
 trait HasCRUD
 {
+
+    protected function fill()
+    {
+        $fillArray = array();
+        foreach ($this->fillabe as $attribute) {
+            if (isset($this->$attribute)) {
+                $fillArray[] = $this->getAttributeName($attribute) . " = ? ";
+                $this->inCastsAttributes($attribute) ?
+                    $this->addValue($attribute, $this->castsEncodeValue($attribute, $this->$attribute))
+                    : $this->addValue($attribute, $this->$attribute);
+            }
+        }
+        return implode(' , ', $fillArray);
+    }
+
+    protected function saveMehtod()
+    {
+        $fillString = $this->fill();
+        if (!isset($this->{$this->primaryKey})) {
+            $this->setSql("INSERT INTO" . $this->getTableName() . "SET $fillString, "
+                . $this->getAttributeName($this->createdAt) . "=Now()");
+        }else{
+            $this->setSql("UPDATE ". $this->getTableName() . "SET $fillString, "
+                . $this->getAttributeName($this->updateAt) . "=Now()");
+            $this->setWhere("AND" , $this->getAttributeName($this->primaryKey)." = ?");
+            $this->addValue($this->primaryKey , $this->{$this->primaryKey});
+        }
+        $this->executeQuery();
+    }
+
 
 }
